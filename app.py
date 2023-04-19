77a6fdccab10ebe02d680fde9b2ac3d43702e770
Russell Jones <justyrones@gmail.com>
2023-04-19 17:51:30 -0600

    understanding the problem

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import silly_story, excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
+
+@app.get("/questions")
+def make_questions():
+    """Put the questions on the page"""
+
+    app.request()
+
+    html = render_template("questions.html",place= )
 
 debug = DebugToolbarExtension(app)
