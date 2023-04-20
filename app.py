43b274aee1eeb0dad22e04c43dc1b06d3a729870
Steven Lee <stevelee83@gmail.com>
2023-04-19 17:22:39 -0700

    work on auto-generated fields for form

--- app.py
+++ app.py
@@ -6,24 +6,25 @@ from stories import silly_story, excited_story
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
-@app.get("/questions")
+@app.get("/")
 def make_questions():
+
     """Put the questions on the page"""
 
-    place = request.args.get("place")
-    noun = request.args.get("noun")
-    verb = request.args.get("verb")
-    adjective = request.args.get("adjective")
-    plural_noun = request.args.get("plural_noun")
 
 
-    html = render_template("results.html",place=place
-    noun=noun, verb=verb, adjective=adjective, plural_noun=plural_noun)
+    html = render_template("questions.html", prompt_list=silly_story.prompts)
+    #                        place=place,
+    # noun=noun, verb=verb, adjective=adjective, plural_noun=plural_noun)
 
     return html
 
 @app.get("/results")
 def make_results():
-
+    place = request.args.get("place")
+    noun = request.args.get("noun")
+    verb = request.args.get("verb")
+    adjective = request.args.get("adjective")
+    plural_noun = request.args.get("plural_noun")
 
 debug = DebugToolbarExtension(app)
